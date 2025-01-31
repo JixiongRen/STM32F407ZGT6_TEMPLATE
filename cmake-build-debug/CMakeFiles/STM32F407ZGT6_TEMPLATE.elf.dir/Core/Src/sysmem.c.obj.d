5990a0a217da6f8a874405782fb7c3f89a86945d
Jixiong Ren <renjixiong@126.com>
2025-01-31 16:42:05 +0800

    stm32f407zgt6开发模板，基于CubeMX+CLion

---
+++ cmake-build-debug/CMakeFiles/STM32F407ZGT6_TEMPLATE.elf.dir/Core/Src/sysmem.c.obj.d
@@ -0,0 +1,20 @@
+CMakeFiles/STM32F407ZGT6_TEMPLATE.elf.dir/Core/Src/sysmem.c.obj: \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Core\Src\sysmem.c \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/errno.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/errno.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/reent.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/_ansi.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/newlib.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/_newlib_version.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/config.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/machine/ieeefp.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/features.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stddef.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_types.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/machine/_types.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/machine/_default_types.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/lock.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_intsup.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_stdint.h
---
+++ cmake-build-debug/CMakeFiles/STM32F407ZGT6_TEMPLATE.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d
@@ -0,0 +1,36 @@
+CMakeFiles/STM32F407ZGT6_TEMPLATE.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj: \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Src\stm32f4xx_hal_pwr_ex.c \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Core\Inc/stm32f4xx_hal_conf.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_def.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f4xx.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Device\ST\STM32F4xx\Include/stm32f407xx.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Include/core_cm4.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/stdint.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/machine/_default_types.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/features.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/_newlib_version.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_intsup.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/../../../../arm-none-eabi/include/sys/_stdint.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Include/cmsis_version.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Include/cmsis_compiler.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Include/cmsis_gcc.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Include/mpu_armv7.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\CMSIS\Device\ST\STM32F4xx\Include/system_stm32f4xx.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ C:\ST\STM32CubeCLT_1.16.0\GNU-tools-for-STM32\bin\../lib/gcc/arm-none-eabi/12.3.1/include/stddef.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_rcc_ex.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_gpio_ex.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_exti.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_dma_ex.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_cortex.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ex.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_pwr_ex.h \
+ C:\Users\renji\CLionProjects\STM32F407ZGT6_TEMPLATE\Drivers\STM32F4xx_HAL_Driver\Inc/stm32f4xx_hal_uart.h
